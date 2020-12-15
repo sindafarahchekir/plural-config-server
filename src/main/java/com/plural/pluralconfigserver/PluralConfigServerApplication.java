043e40ffe69f9ff6a9070a4283954a4128f75899
SINDA <sendafara.chkir@nearteam.fr>
2020-12-15 12:02:50 +0100

    first commit

---
+++ src/main/java/com/plural/pluralconfigserver/PluralConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.plural.pluralconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plural/pluralconfigserver/PluralConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plural.pluralconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
